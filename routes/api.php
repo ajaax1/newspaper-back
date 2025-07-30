240c92c1ee282a006bd85840f422113e627f6a17
Ruan Higor Silva <ruanhigor123@gmail.com>
2025-07-30 17:50:29 -0300

    tirar refresh

--- routes/api.php
+++ routes/api.php
@@ -19,7 +19,6 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::post('login', [AuthController::class, 'login']);
-Route::post('refresh', [AuthController::class, 'refresh']);
 
 Route::middleware('auth:sanctum')->group(function () {
     Route::post('logout', [AuthController::class, 'logout']);
